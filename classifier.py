7b4c8269ff2d48d7cae83f49d8794d8332038186
mrc <1900840490lhx@gmail.com>
2025-11-10 06:17:57 +1100

    clear useless comment.

--- classifier.py
+++ classifier.py
@@ -4,9 +4,9 @@ model_path = "svm_model.joblib"
 def svm(X_train, y_train,C = 1.0):
     clf = LinearSVC(
         C=C,
-        class_weight='balanced',  # 如果类别不均衡，自动给少数类更高权重
+        class_weight='balanced',
         max_iter=10000,
-        dual=False  # 对样本数 >> 特征数很有用；你可以试试 True/False 哪个更好
+        dual=False
     )
     print("Start training SVM...")
     clf.fit(X_train, y_train)
